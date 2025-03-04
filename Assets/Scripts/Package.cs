dfd76ab0408db2b0f7911c4353bf43cc01824540
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-03-04 13:25:18 +0100

    removed unused functions and variables

--- Assets/Scripts/Package.cs
+++ Assets/Scripts/Package.cs
@@ -2,18 +2,4 @@ using UnityEngine;
 
 public class Package : MonoBehaviour
 {
-    public string PackageName;
-    public int PackageVersion;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
