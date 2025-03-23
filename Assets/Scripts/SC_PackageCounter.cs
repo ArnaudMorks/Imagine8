5b02a904f526d74731f9c40034978b7dfc05d90f
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-03-23 14:32:51 +0100

    added basic counter with visual indiactor

---
+++ Assets/Scripts/SC_PackageCounter.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_PackageCounter : MonoBehaviour
+{
+    [SerializeField] private GameObject _visualIndecators;
+    private List<GameObject> _indecators;
+    private int _packageAmount;
+
+    private SC_PackageSpawner _spawner;
+
+    private void Awake()
+    {
+        _spawner = FindAnyObjectByType<SC_PackageSpawner>();
+        _spawner.OnDeSpawnedPackage += UpdateCounter;
+
+        _indecators = GetChildren();
+    }
+
+    private void UpdateCounter(GameObject package)
+    {
+        _packageAmount = _spawner.packageAmount - 1;
+        var deactivateAmount = _indecators.Count / _packageAmount;
+
+        DeActivateObjects(_indecators, deactivateAmount);
+    }
+
+    private List<GameObject> GetChildren()
+    {
+        List<GameObject> children = new List<GameObject>();
+        Transform[] allChildren = _visualIndecators.GetComponentsInChildren<Transform>();
+
+        foreach (var child in allChildren)
+        {
+            if (child.parent == _visualIndecators.transform)
+                children.Add(child.gameObject);
+        }
+
+        return children;
+    }
+
+    private void DeActivateObjects(List<GameObject> objectList, int amount)
+    {
+        int deactivatedAmount = 0;
+        foreach (var item in objectList)
+        {
+            if (deactivatedAmount >= amount) break;
+
+            if (item.activeSelf)
+            {
+                Debug.Log(item);
+                item.SetActive(false);
+                deactivatedAmount++;
+                continue;
+            }
+        }
+    }
+}
--- Assets/Scripts/SC_PackageSpawner.cs
+++ Assets/Scripts/SC_PackageSpawner.cs
@@ -11,11 +11,17 @@ public class SC_PackageSpawner : MonoBehaviour
     private int _currentPackage = 0;
     private GameObject _lastPackage;
 
+    public int packageAmount { get; private set; }
+
     public Action OnSpawnedPackage;
     public Action<GameObject> OnDeSpawnedPackage;
     public Action OnFinishList;
 
-    private void Awake() => _packages = _packageList.GetComponentsInChildren<SC_Package>();
+    private void Awake()
+    {
+        _packages = _packageList.GetComponentsInChildren<SC_Package>();
+        packageAmount = _packages.Length;
+    }
 
     //REMOVE WHEN THERE IS A GAME LOOP
     public void Start() => TrySpawnPackage();
@@ -50,12 +56,12 @@ public class SC_PackageSpawner : MonoBehaviour
 
     private void SpawnPackage()
     {
-        OnSpawnedPackage?.Invoke();
-
         _lastPackage = Instantiate(_packages[_currentPackage].gameObject,
             _startPosition.transform.position, Quaternion.identity);
 
         _currentPackage++;
+
+        OnSpawnedPackage?.Invoke();
     }
 
     private void DeSpawnPackage()
