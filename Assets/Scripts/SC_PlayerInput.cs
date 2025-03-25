c7917d6fb2224cf2fdd52bcb9b8aa2740e7e663f
ArnaudMorks <arnaud.morks@gmail.com>
2025-03-25 22:39:04 +0100

    Not important, this is a useless branch

--- Assets/Scripts/SC_PlayerInput.cs
+++ Assets/Scripts/SC_PlayerInput.cs
@@ -3,67 +3,25 @@ using UnityEngine.InputSystem;
 
 public class SC_PlayerInput : MonoBehaviour
 {
-	private const int SCREEN_WIDTH = 1920;
-	private const int SCREEN_HEIGHT = 1080;
-	[SerializeField] private Vector3 g_mousePosition;
-	[SerializeField] private bool g_hasStamp = false;		//change to ENUM system later
+	[SerializeField] private Collider g_stampOverInkTriggerArea;
+	[SerializeField] private Transform g_stampOverInkTransform;
 
-	//assumes stamps are a grid with 4 points
-	//put "InkPlayField" positions in the "g_stampPositions" array in the correct place
-	//"0" = left up, "1" = right up, "2" = left down, "3" = right down
-	[SerializeField] private Vector2[] g_mouseStampPositions;
-
-	//put "X" and "Z" world positions in the array, DOES NOT affect "Y" position
-	//this will set the boundries to move the "StampColorPickPivot"
-	//"0" = left up, "1" = right up, "2" = left down, "3" = right down
-	[SerializeField] private Vector3[] g_worldStampPositions;
-	[SerializeField] private float g_allWorldYStampPosition;
-
-	[SerializeField] private Vector2[] g_mouseToWorldEdgePositions;
-
-	[SerializeField] private Vector2 g_currentMouseToWorldOffset;
-
-
-	private void Start()
-	{
-		g_mouseStampPositions = SetStampCanvasToMousePositions(g_mouseStampPositions);
-		g_mouseToWorldEdgePositions = SetMouseToWorldEdgePositions(g_mouseToWorldEdgePositions,
-			g_mouseStampPositions, g_worldStampPositions);
-	}
 
 	private void Update()
 	{
-		g_mousePosition = Mouse.current.position.ReadValue();
-		print(g_mousePosition);
-
-		
-
-	}
-
-
-	Vector2[] SetStampCanvasToMousePositions(Vector2[] stampPositions)
-	{
-		//canvas width position is opposite of mouse position, therefore this for loop
-		//with canvas height "0" is in the middle of the screen
-		for (int i = 0; i < 4; i++)		//"4" because there are four stamp positions
+		if (Mouse.current.leftButton.isPressed)
 		{
-			stampPositions[i].x += SCREEN_WIDTH;
-			stampPositions[i].y += SCREEN_HEIGHT / 2;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (g_stampOverInkTriggerArea.Raycast(ray, out hit, 100.0f))
+			{
+				g_stampOverInkTransform.position = ray.GetPoint(100.0f);
+			}
 		}
 
-		return stampPositions;
 	}
 
-	Vector2[] SetMouseToWorldEdgePositions(Vector2[] mouseToWorldEdgePositions,
-		Vector2[] mouseEdgePositions, Vector3[] worldEdgePositions)
-	{
-		for (int i = 0; i < 4; i++)		//"4" because there are four stamp positions
-		{
-			mouseToWorldEdgePositions[i].x = mouseEdgePositions[i].x / worldEdgePositions[i].x;
-			mouseToWorldEdgePositions[i].y = mouseEdgePositions[i].y / worldEdgePositions[i].z;
-		}
 
-		return mouseToWorldEdgePositions;
-	}
 
 }
