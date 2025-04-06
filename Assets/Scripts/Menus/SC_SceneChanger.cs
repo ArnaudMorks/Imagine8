cf85b971d43e58f50bed72988faa24153d98b4c1
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-04-06 11:47:33 +0200

    fixed timer stopping after leaving scene

--- Assets/Scripts/Menus/SC_SceneChanger.cs
+++ Assets/Scripts/Menus/SC_SceneChanger.cs
@@ -1,9 +1,38 @@
+using TimeSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SC_SceneChanger : MonoBehaviour
 {
-    public void ChangeScene(string scene) => SceneManager.LoadScene(scene);
+    private SC_TimeManager _timeManager;
+
+
+    public void ChangeScene(string scene)
+    {
+        TryToAssignTimeManager();
+        ResetSigletonsValues();
+
+        SceneManager.LoadScene(scene);
+    }
 
     public void Quit() => Application.Quit();
+
+
+    private void TryToAssignTimeManager()
+    {
+        if (SC_TimeManager.Instance == null)
+        {
+            Debug.LogWarning("TimeManager instance is missing!");
+            return;
+        }
+
+        _timeManager = SC_TimeManager.Instance;
+    }
+
+    private void ResetSigletonsValues()
+    {
+        _timeManager.RunningIngameTime(true);
+
+        //Expand...
+    }
 }
