521b168db4066c1a4f803e245a7dc0c9724c636f
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-04-08 10:15:04 +0200

    added sound and rearanged 1 stamp position

--- Assets/Scripts/InputHandler/StampInputs/SC_InteractHolderStamp.cs
+++ Assets/Scripts/InputHandler/StampInputs/SC_InteractHolderStamp.cs
@@ -6,75 +6,80 @@
 //
 
 
-using CameraSystem;
 using UnityEngine;
 
 [RequireComponent(typeof(SC_TemporaryItemInteractReceiver))]
 public class SC_InteractHolderStamp : MonoBehaviour
 {
-	// My item interact receiver
-	private SC_TemporaryItemInteractReceiver g_itemInteractReceiver;
+    // My item interact receiver
+    private SC_TemporaryItemInteractReceiver g_itemInteractReceiver;
 
-	//"0" = turn to left, "1" = turn to right
-	[SerializeField] private int g_turnToDirection;
+    //"0" = turn to left, "1" = turn to right
+    [SerializeField] private int g_turnToDirection;
 
-	[SerializeField] private SC_HolderStampManager g_holderStampManager;
+    [SerializeField] private SC_HolderStampManager g_holderStampManager;
 
-
-	// ----------------- Functions -----------------
-
-
-	#region OnEnable Functions
-
-	// Changes that happen on enable.
-	private void OnEnable()
-	{
-		TryAssignItemInteractReceiver();
-		SubscribeOnReceivedHit();
-
-		// Expand..
-	}
-
-	// Auto-assign the item interact receiver.
-	private void TryAssignItemInteractReceiver()
-	{
-		if (this.TryGetComponent(out SC_TemporaryItemInteractReceiver itemInteractReceiver))
-			g_itemInteractReceiver = itemInteractReceiver;
-		else
-			Debug.LogWarning("Unable to get my _itemInteractReceiver component!");
-	}
+    [Header("Audio")]
+    [SerializeField] private AudioSource g_audioSource;
 
 
-	#endregion
+    // ----------------- Functions -----------------
 
-	#region OnDisable Functions
 
-	// Changes that happen on disable.
-	private void OnDisable()
-	{
-		UnSubscribeOnReceivedHit();
+    #region OnEnable Functions
 
-		// Expand..
-	}
+    // Changes that happen on enable.
+    private void OnEnable()
+    {
+        TryAssignItemInteractReceiver();
+        SubscribeOnReceivedHit();
 
-	#endregion
+        // Expand..
+    }
 
-	#region Subscription Functions
+    // Auto-assign the item interact receiver.
+    private void TryAssignItemInteractReceiver()
+    {
+        if (this.TryGetComponent(out SC_TemporaryItemInteractReceiver itemInteractReceiver))
+            g_itemInteractReceiver = itemInteractReceiver;
+        else
+            Debug.LogWarning("Unable to get my _itemInteractReceiver component!");
+    }
 
-	// On received hit subscription.
-	private void SubscribeOnReceivedHit() => g_itemInteractReceiver.OnReceivedHit += ReceivedResult;
-	private void UnSubscribeOnReceivedHit() => g_itemInteractReceiver.OnReceivedHit -= ReceivedResult;
 
-	// On received hit result.
-	private void ReceivedResult()
-	{
-		TurnToSideInteract();
-	}
+    #endregion
 
-	private void TurnToSideInteract()
-	{
-		g_holderStampManager.TurnHolderStamp(g_turnToDirection);
-	}
+    #region OnDisable Functions
 
-	#endregion
+    // Changes that happen on disable.
+    private void OnDisable()
+    {
+        UnSubscribeOnReceivedHit();
+
+        // Expand..
+    }
+
+    #endregion
+
+    #region Subscription Functions
+
+    // On received hit subscription.
+    private void SubscribeOnReceivedHit() => g_itemInteractReceiver.OnReceivedHit += ReceivedResult;
+    private void UnSubscribeOnReceivedHit() => g_itemInteractReceiver.OnReceivedHit -= ReceivedResult;
+
+    // On received hit result.
+    private void ReceivedResult()
+    {
+        TurnToSideInteract();
+    }
+
+    private void TurnToSideInteract()
+    {
+        if (g_audioSource != null)
+            g_audioSource.Play();
+
+        g_holderStampManager.TurnHolderStamp(g_turnToDirection);
+    }
+
+    #endregion
 }
--- Assets/Scripts/SC_StampManager.cs
+++ Assets/Scripts/SC_StampManager.cs
@@ -4,123 +4,131 @@ using UnityEngine;
 //LOT OF REPEATING CODE; IMPROVE LATER
 public class SC_StampManager : MonoBehaviour
 {
-	//Takes all stamp pickup hitboxes from high to low
-	[SerializeField] private SC_InteractStampPickup[] g_interactStampPickups;
+    //Takes all stamp pickup hitboxes from high to low
+    [SerializeField] private SC_InteractStampPickup[] g_interactStampPickups;
 
-	[SerializeField] private SC_StampStateEnum g_stampState;
-	[SerializeField] private PackageStampIcon g_iconPackageStamp;
-	[SerializeField] private PackageStampColor g_colorStampEnum;
+    [SerializeField] private SC_StampStateEnum g_stampState;
+    [SerializeField] private PackageStampIcon g_iconPackageStamp;
+    [SerializeField] private PackageStampColor g_colorStampEnum;
 
-	[SerializeField] private SC_MovingStamp g_movingStampScript;
-	[SerializeField] private SC_VisualStamp g_visualStampScript;
-	[SerializeField] private SC_VisualHolderStamps g_visualHolderScript;
+    [SerializeField] private SC_MovingStamp g_movingStampScript;
+    [SerializeField] private SC_VisualStamp g_visualStampScript;
+    [SerializeField] private SC_VisualHolderStamps g_visualHolderScript;
 
-	[SerializeField] private SC_Package g_latestPackage;
+    [SerializeField] private SC_Package g_latestPackage;
 
-	[Header("Timers")]
-	[SerializeField] private float g_timeInkStamping;
-	[SerializeField] private float g_timeOnPackageStamping;
+    [Header("Timers")]
+    [SerializeField] private float g_timeInkStamping;
+    [SerializeField] private float g_timeOnPackageStamping;
 
-	[Header("Parcel View")]
-	[SerializeField] private SC_CameraView g_parcelView;
+    [Header("Parcel View")]
+    [SerializeField] private SC_CameraView g_parcelView;
 
-	[Header("Main View")]
-	[SerializeField] private SC_CameraView g_mainView;
+    [Header("Main View")]
+    [SerializeField] private SC_CameraView g_mainView;
 
+    [Header("Audio")]
+    [SerializeField] private AudioSource g_audioSource;
 
-	//Specific function to invoke because of timer
-	private void SetToHasInk()
-	{
-		g_stampState = SC_StampStateEnum.HAS_INK;
-		SetMoveStampState(g_stampState);
-		g_visualStampScript.SetCurrentAnimationStamp(0);
+    //Specific function to invoke because of timer
+    private void SetToHasInk()
+    {
+        g_stampState = SC_StampStateEnum.HAS_INK;
+        SetMoveStampState(g_stampState);
+        g_visualStampScript.SetCurrentAnimationStamp(0);
 
-		SC_CameraManager.Instance.VieverToNewView(g_parcelView);
-	}
+        SC_CameraManager.Instance.VieverToNewView(g_parcelView);
+    }
 
-	private void SetOnPackageStamp()
-	{
-		//Adds color and icon to the package
-		g_latestPackage.AddStamp(g_colorStampEnum, g_iconPackageStamp);
+    private void SetOnPackageStamp()
+    {
+        //Adds color and icon to the package
+        g_latestPackage.AddStamp(g_colorStampEnum, g_iconPackageStamp);
 
-		SC_CameraManager.Instance.VieverToNewView(g_mainView);
+        SC_CameraManager.Instance.VieverToNewView(g_mainView);
 
-		TryFinishStamp();
-	}
+        TryFinishStamp();
+    }
 
-	private void TryFinishStamp()
-	{
-		if (g_stampState != SC_StampStateEnum.NOT_HOLDING)
-		{
-			//stamp on package FOR LATER
-			g_stampState = SC_StampStateEnum.NOT_HOLDING;
+    private void TryFinishStamp()
+    {
+        if (g_stampState != SC_StampStateEnum.NOT_HOLDING)
+        {
+            //stamp on package FOR LATER
+            g_stampState = SC_StampStateEnum.NOT_HOLDING;
 
-			SetMoveStampState(g_stampState);
+            SetMoveStampState(g_stampState);
 
-			//Enables and disables correct visuals
-			g_visualHolderScript.EnableStampOnHolder();
-			g_visualStampScript.DisableStampVisual();
-		}
-	}
-
-
-	private void SetMoveStampState(SC_StampStateEnum stampState)
-	{
-		g_movingStampScript.MovingStampState = stampState;
-		g_movingStampScript.TrySnapToPosition();
-	}
+            //Enables and disables correct visuals
+            g_visualHolderScript.EnableStampOnHolder();
+            g_visualStampScript.DisableStampVisual();
+        }
+    }
 
 
-	//Tries to grab a stamp, and put's a stamp back when already holding one (when possible)
-	public void TryGettingSymbol(PackageStampIcon iconStampState, bool canPickupStamp)
-	{
-		if (g_stampState == SC_StampStateEnum.NOT_HOLDING && canPickupStamp)
-		{
-			g_iconPackageStamp = iconStampState;
-			g_stampState = SC_StampStateEnum.OVER_INK_MOVE;
+    private void SetMoveStampState(SC_StampStateEnum stampState)
+    {
+        g_movingStampScript.MovingStampState = stampState;
+        g_movingStampScript.TrySnapToPosition();
+    }
 
-			SetMoveStampState(g_stampState);
-			//g_visualStampScript.SetCurrentAnimationStamp(g_stampState, g_colorStampEnum);
 
-			//Disables and enables correct visuals
-			g_visualHolderScript.DisableStampOnHolder(iconStampState);
-			g_visualStampScript.EnableStampVisual(iconStampState);
-		}
-		else if (g_stampState != SC_StampStateEnum.GETTING_INK
-			&& g_stampState != SC_StampStateEnum.STAMP_ON_PACKAGE)
-		{
-			//put stamp back if holding one
-			TryFinishStamp();
-		}
-	}
+    //Tries to grab a stamp, and put's a stamp back when already holding one (when possible)
+    public void TryGettingSymbol(PackageStampIcon iconStampState, bool canPickupStamp)
+    {
+        if (g_stampState == SC_StampStateEnum.NOT_HOLDING && canPickupStamp)
+        {
+            g_iconPackageStamp = iconStampState;
+            g_stampState = SC_StampStateEnum.OVER_INK_MOVE;
 
-	//triggers when clicking on an ink color while holding the stamp over said color
-	public void TryGettingSpecificColor(PackageStampColor stampColorEnum)
-	{
-		if (g_stampState == SC_StampStateEnum.OVER_INK_MOVE)
-		{
-			g_colorStampEnum = stampColorEnum;
-			g_stampState = SC_StampStateEnum.GETTING_INK;
+            SetMoveStampState(g_stampState);
+            //g_visualStampScript.SetCurrentAnimationStamp(g_stampState, g_colorStampEnum);
 
-			SetMoveStampState(g_stampState);
-			g_movingStampScript.TrySetCurrentColorPosition(stampColorEnum);
-			g_visualStampScript.SetCurrentInk(stampColorEnum);
-			Invoke("SetToHasInk", g_timeInkStamping);
-		}
-	}
+            //Disables and enables correct visuals
+            g_visualHolderScript.DisableStampOnHolder(iconStampState);
+            g_visualStampScript.EnableStampVisual(iconStampState);
+        }
+        else if (g_stampState != SC_StampStateEnum.GETTING_INK
+            && g_stampState != SC_StampStateEnum.STAMP_ON_PACKAGE)
+        {
+            //put stamp back if holding one
+            TryFinishStamp();
+        }
+    }
 
-	//triggers when clicking on a package while there is ink on the stamp
-	public void TryOnPackageStamp(SC_Package currentPackage)
-	{
-		if (g_stampState == SC_StampStateEnum.HAS_INK)
-		{
-			g_stampState = SC_StampStateEnum.STAMP_ON_PACKAGE;
-			g_latestPackage = currentPackage;
-			g_visualStampScript.SetCurrentAnimationStamp(1);
-			g_movingStampScript.MovingStampState = g_stampState;
-			Invoke("SetOnPackageStamp", g_timeOnPackageStamping);
-		}
-	}
+    //triggers when clicking on an ink color while holding the stamp over said color
+    public void TryGettingSpecificColor(PackageStampColor stampColorEnum)
+    {
+        if (g_stampState == SC_StampStateEnum.OVER_INK_MOVE)
+        {
+            if (g_audioSource != null)
+                g_audioSource.Play();
+
+            g_colorStampEnum = stampColorEnum;
+            g_stampState = SC_StampStateEnum.GETTING_INK;
+
+            SetMoveStampState(g_stampState);
+            g_movingStampScript.TrySetCurrentColorPosition(stampColorEnum);
+            g_visualStampScript.SetCurrentInk(stampColorEnum);
+            Invoke("SetToHasInk", g_timeInkStamping);
+        }
+    }
+
+    //triggers when clicking on a package while there is ink on the stamp
+    public void TryOnPackageStamp(SC_Package currentPackage)
+    {
+        if (g_stampState == SC_StampStateEnum.HAS_INK)
+        {
+            if (g_audioSource != null)
+                g_audioSource.Play();
+
+            g_stampState = SC_StampStateEnum.STAMP_ON_PACKAGE;
+            g_latestPackage = currentPackage;
+            g_visualStampScript.SetCurrentAnimationStamp(1);
+            g_movingStampScript.MovingStampState = g_stampState;
+            Invoke("SetOnPackageStamp", g_timeOnPackageStamping);
+        }
+    }
 
 
 }
